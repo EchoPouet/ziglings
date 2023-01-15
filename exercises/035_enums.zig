3a3ecce0bac321f400f20c07818374219559b4a2
Arnaud Moura <arnaudmoura@gmail.com>
2023-01-15 14:00:35 +0100

    fix: 35 and 36

--- exercises/035_enums.zig
+++ exercises/035_enums.zig
@@ -20,7 +20,7 @@
 const std = @import("std");
 
 // Please complete the enum!
-const Ops = enum { ??? };
+const Ops = enum { inc, pow, dec };
 
 pub fn main() void {
     const operations = [_]Ops{
@@ -44,8 +44,7 @@ pub fn main() void {
             },
             Ops.pow => {
                 current_value *= current_value;
-            },
-            // No "else" needed! Why is that?
+            }
         }
 
         std.debug.print("{} ", .{current_value});
